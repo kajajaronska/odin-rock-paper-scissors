4f2ef7569bdcfd40cd619cf885f4dfcef1db24c1
Kaja Jaronska <kaja.jaronska@gmail.com>
2021-11-05 16:41:22 +0000

    Scoreboard is now updating with current scores for both players

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const enemyScore = document.querySelector('.enemy-score');
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random()*3 + 1);
-
     if(randomNumber === 1) return "Rock";
     if(randomNumber === 2) return "Paper";
     if(randomNumber === 3) return "Scissors";
@@ -19,9 +18,6 @@ function computerPlay() {
 
 /////////////////////////////
 // User selection setup
-
-
-// Function to set user's choice on 'click' event 
 
 let userChoice;
 
@@ -42,87 +38,84 @@ scissors.addEventListener('click', () => {
 
 
 /////////////////////////////
-// Single round function
+// Single round function and score counter
 
-let winner;
-let computerSelection;
-let playerSelection;
+
+let enemyScoreCounter = 0;
+let playerScoreCounter = 0;
+
 
 function playRound() {
 
-    computerSelection = computerPlay();
-    playerSelection = userChoice;
+    computerChoice = computerPlay();
 
-    console.log(`Computer\'s selection: ${computerSelection} | Player\'s selection: ${playerSelection}`);
+    console.log(`Computer\'s selection: ${computerChoice} | Player\'s selection: ${userChoice}`);
 
-    
         switch(true) {
-            case computerSelection === "Rock" && playerSelection === "Scissors":
-            case computerSelection === "Scissors" && playerSelection === "Paper":
-            case computerSelection === "Paper" && playerSelection === "Rock":
-            winner = "Computer";
-            console.log(`${computerSelection} beats ${playerSelection}. You lose!😱`)
-            enemyScore.textContent = '1';
+            case computerChoice === "Rock" && userChoice === "Scissors":
+            case computerChoice === "Scissors" && userChoice === "Paper":
+            case computerChoice === "Paper" && userChoice === "Rock":
+            console.log(`${computerChoice} beats ${userChoice}. You lose!😱`)
+            enemyScoreCounter++;
+            enemyScore.textContent = enemyScoreCounter.toString();
+            console.log(enemyScoreCounter, playerScoreCounter);
+
             break;
     
-            case playerSelection === "Rock" && computerSelection === "Scissors":
-            case playerSelection === "Scissors" && computerSelection === "Paper":
-            case playerSelection === "Paper" && computerSelection === "Rock":
-            winner = "Player";
-            console.log(`${playerSelection} beats ${computerSelection}. You win!👾`)
-            userScore.textContent = '1';
+            case userChoice === "Rock" && computerChoice === "Scissors":
+            case userChoice === "Scissors" && computerChoice === "Paper":
+            case userChoice === "Paper" && computerChoice === "Rock":
+            console.log(`${userChoice} beats ${computerChoice}. You win!👾`)
+            playerScoreCounter++;
+            userScore.textContent = playerScoreCounter.toString();
+            console.log(enemyScoreCounter, playerScoreCounter);
             break;
     
-            case playerSelection === computerSelection:
-            winner = "";
+            case userChoice === computerChoice:
             console.log("Draw! Try again 🤷‍♂️");
+            console.log(enemyScoreCounter, playerScoreCounter);
             break;
     
             default:
                 console.log("Whoops! Something went horribly wrong ... Reload?")
-    
         };
         
     return;
 };
 
-// playRound();
-
 ///////////////////////////
 // 5 round game function
 
-function game() {
-    // Setting initial scores to 0
-    computerScore = 0;
-    playerScore = 0;
+// function game() {
+//     // Setting initial scores to 0
+//     computerScore = 0;
+//     playerScore = 0;
 
-    // Looping 5 times for 5 rounds
-    for (let i=1; i <= 5; i++) {
-        console.log(`Round:${i}`)
-        playRound();
+//     // Looping 5 times for 5 rounds
+//     for (let i=1; i <= 5; i++) {
+//         console.log(`Round:${i}`)
+//         playRound();
 
         
-        if(winner === "Computer"){
-            computerScore += 1;
-            console.log(`---- Computer: ${computerScore} | Player: ${playerScore} ----`);
-        } else if(winner === "Player"){
-            playerScore += 1
-            console.log(`Computer: ${computerScore} | Player: ${playerScore}`);
-        } else {
-            console.log(`Computer: ${computerScore} | Player: ${playerScore}`);
-        }
+//         if(winner === "Computer"){
+//             computerScore += 1;
+//             console.log(`---- Computer: ${computerScore} | Player: ${playerScore} ----`);
+//         } else if(winner === "Player"){
+//             playerScore += 1
+//             console.log(`Computer: ${computerScore} | Player: ${playerScore}`);
+//         } else {
+//             console.log(`Computer: ${computerScore} | Player: ${playerScore}`);
+//         }
+//     } 
 
+//     // Announcing final score after last round 
+//     if(computerScore > playerScore) console.log(`Computer won this game!`);
+//     else if(playerScore > computerScore) console.log(`Well done! You won this game!`);
+//     else{
+//         console.log(`It\'s a draw!`)
+//     }
 
-    } 
-
-    // Announcing final score after last round 
-    if(computerScore > playerScore) console.log(`Computer won this game!`);
-    else if(playerScore > computerScore) console.log(`Well done! You won this game!`);
-    else{
-        console.log(`It\'s a draw!`)
-    }
-
-    return;
-}
+//     return;
+// }
 
 // game();
